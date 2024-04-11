81b86315d62e218cb45f985d2665ea26837e30bf
moli <kyeung516@gmail.com>
2024-04-12 01:02:06 +0800

    12/4 update

--- main.py
+++ main.py
@@ -2,13 +2,30 @@ import sys
 import random
 import datetime
 
+sys.setrecursionlimit(10000)
+
 record = []
 print("Caesar cipher encrypt and decrypt service\ntype '/help' for help")
+
+f = open("vocab.txt","r")
+vocab = f.read().split("\n")
 
 def checkHist(arr):
     for i in range(len(arr)):
         print("Record " + str(i + 1) + ":\nTime: " + arr[i][1] + "\nOperation Type: " + arr[i][0] + "\nInput: " + arr[i][2] + "\nOutput: " + arr[i][3])
     
+def checkVocab(s, type):
+    outputVocab = s.split(" ")
+    cnt = 0
+    for i in outputVocab:
+        for j in vocab:
+            if(i == j):
+                cnt += 1
+                print(i)
+                if(cnt > len(outputVocab) / 6):
+                    return True
+    decrypt(s, 1, type)
+
 def isSmall(k):
     if(k > 96 and k < 123): return True
     return False
@@ -19,7 +36,11 @@ def isLetter(k):
 
 def outputProcess(s, type):
     if(type == 1):
-        g = open("result.txt", "w")
+        try:
+            g = open("result.txt", "w")
+        except:
+            g = open("result.txt", "x")
+            g = open("result.txt", "w")
         g.write(s)
         g.close
         print("Done!")
@@ -40,8 +61,7 @@ def encrypt(s, type):
     record.append(["Encryption", time.strftime("%d/%m/%Y - %H:%M:%S"), s, output])
     outputProcess(output, type)
 
-def decrypt(s, type):
-    output = ""
+def findDiff(s, type):
     cnt = [0] * 26
     for i in range(len(s)):
         intChar = ord(s[i])
@@ -54,18 +74,23 @@ def decrypt(s, type):
     maxIndex = 0
     for i in range(25):
         if(cnt[i + 1] > cnt[maxIndex] and i != 3): maxIndex = i + 1
-    different = maxIndex - 4
-    if(different < 0) : different += 26
+    diff = maxIndex - 4
+    if(diff < 0) : diff += 26
+    decrypt(s, diff, type)
+    
+def decrypt(s, k, type):
+    output = ""
     for i in range(len(s)):
         intChar = ord(s[i])
         if(isLetter(intChar)):
-            if((intChar - different < 97 and isSmall(intChar)) or (intChar - different < 65 and not(isSmall(intChar)))): newAscii = intChar - different + 26
-            else: newAscii = intChar - different
+            if((intChar - k < 97 and isSmall(intChar)) or (intChar - k < 65 and not(isSmall(intChar)))): newAscii = intChar - k + 26
+            else: newAscii = intChar - k
             output += chr(newAscii)
         else: output += s[i]
-    time = datetime.datetime.now()
-    record.append(["Decryption", time.strftime("%d/%m/%Y - %H:%M:%S"), s, output])
-    outputProcess(output, type)
+    if(len(output.split(" ")) > 200 or checkVocab(output, type) == True):
+        time = datetime.datetime.now()
+        record.append(["Decryption", time.strftime("%d/%m/%Y - %H:%M:%S"), s, output])
+        outputProcess(output, type)
 
 while True:
     command = input("Command: ")
@@ -95,7 +120,7 @@ while True:
             if(commandList[0] == "/decrypt"):
                 if(commandList[1] == "2"):
                     content = input("Input the text you want to decrypt: ")
-                decrypt(content, int(commandList[1]))
+                findDiff(content, int(commandList[1]))
             else:
                 if(commandList[1] == "2"):
                     content = input("Input the text you want to encrypt: ")
